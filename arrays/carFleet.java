ae2c43cf34b3441467fcb5f447e3f60c18435827
fhannan <fhannan@FHANNAN-7420.us.oracle.com>
2022-03-14 13:45:47 +0530

    cp first

---
+++ arrays/carFleet.java
@@ -0,0 +1,11 @@
+// steps:
+
+// 1. sort the given position array in ascending order.
+// 2. calculate time taken by each car to reach the target.
+// 3. store the time taken by last car in a var lets say prev
+// 4. loop through the second last element of the position array
+// 5. check whether the time taken by this car is less than the prev then they will form a fleet.
+// 6. update the prev as the time taken by that last fastest car which is the current one.
+
+
+
